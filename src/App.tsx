9315f3d16a5e558be0ab62cf1874759b43012bd4
albertmanuel <albertmanuels10@gmail.com>
2025-08-17 19:48:14 +0700

    feat: setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="container mx-auto p-4 min-h-svh pt-10">
-        <h1 className="text-lg md:text-4xl">Dashboard Users</h1>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Dashboard Users</h1>
+    </div>
+  );
+};
+
+export default HomePage;
