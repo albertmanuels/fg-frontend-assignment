7374964085d1a53f8ec8680e37060321070b9e80
albertmanuel <albertmanuels10@gmail.com>
2025-08-18 02:06:06 +0700

    fix: lint fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+
 
 function App() {
   return (
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,10 @@
+import UserTable from './components/UserTable';
+
 const HomePage = () => {
   return (
     <div>
       <h1 className="text-4xl font-bold">Dashboard Users</h1>
+      <UserTable />
     </div>
   );
 };
